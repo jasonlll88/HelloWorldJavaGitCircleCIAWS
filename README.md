879d633782fbcdb75f4720712af1df079f2fc94f
Jheison L Mac <jasonlll88@hotmail.com>
2017-09-25 18:37:45 -0500

    Starting repo

---
+++ README.md
@@ -0,0 +1,2 @@
+This is a little example of how to create a java project using as a VCS Github, Continuous integration and testing with circleCI, and deploying the app in AWS in an DockerImage
+
---
+++ src/HelloWorldGitCircleCIAWS.java
@@ -0,0 +1,10 @@
+
+public class HelloWorldGitCircleCIAWS {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World java with github, circleci and aws");
+	}
+
+}
