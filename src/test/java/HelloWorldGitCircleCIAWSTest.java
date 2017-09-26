cb1411a0bf72a7d8f0701961c447ef135c183b6d
Jheison L Mac <jasonlll88@hotmail.com>
2017-09-26 18:41:23 -0500

    Fixing .gitognore file 13

---
+++ src/test/java/HelloWorldGitCircleCIAWSTest.java
@@ -0,0 +1,24 @@
+package test.java;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloWorldGitCircleCIAWSTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);;
+	}
+
+	
+	@Test
+	public void test1() {
+		assertTrue(true);;
+	}
+
+	@Test
+	public void test2() {
+		assertTrue(true);;
+	}
+
+}
