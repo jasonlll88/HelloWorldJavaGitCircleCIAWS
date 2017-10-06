46f009631eaca476c41f88d400280c931c5d427e
Jheison L Rodriguez <jrodr12@bancodebogota.com.co>
2017-10-06 10:36:18 -0500

    Quit execution

--- src/main/java/HelloWorldGitCircleCIAWS.java
+++ src/main/java/HelloWorldGitCircleCIAWS.java
@@ -1,12 +1,17 @@
 package main.java;
 
+import java.util.concurrent.TimeUnit;
+
 public class HelloWorldGitCircleCIAWS {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws InterruptedException {
 
+	
+		while (true) {
 		System.out.println("Hello World java with github, circleci and aws");
-		
 		System.out.println("This has been a change to test the code until circleci");
+		TimeUnit.SECONDS.sleep(1);
+		
+		}
 	}
 }
